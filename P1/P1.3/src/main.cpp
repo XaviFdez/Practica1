0b4eaa2ad1bc82f97c73a0e906a3eb5dc8200259
XaviFdez <160432677+XaviFdez@users.noreply.github.com>
2024-02-28 15:22:29 +0100

    Update main.cpp

--- P1/P1.3/src/main.cpp
+++ P1/P1.3/src/main.cpp
@@ -1,18 +1,34 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED_BUILTIN 23
+#define DELAY 1000
+
+// Dirección base del registro de salida GPIO (ajusta esto según la documentación de tu placa)
+#define GPIO_OUT_REG 0x3FF4400C
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Obtener el puntero al registro de salida GPIO
+  volatile uint32_t *gpio_out = (volatile uint32_t *)GPIO_OUT_REG;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+  // Activar el bit correspondiente al pin 23 (HIGH)
+  *gpio_out |= (1 << LED_BUILTIN);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  Serial.println("ON");
+  
+  // Esperar el tiempo especificado
+  delay(DELAY);
+
+  // Alternar el estado del bit correspondiente al pin 23 (LOW)
+  *gpio_out ^= (1 << LED_BUILTIN);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  Serial.println("OFF");
+
+  // Esperar el tiempo especificado
+  delay(DELAY);
