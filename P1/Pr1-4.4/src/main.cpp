c792cfa9cce55a2e946ef2356631df50849824d5
XaviFdez <160432677+XaviFdez@users.noreply.github.com>
2024-03-10 16:57:53 +0100

    Add files via upload

---
+++ P1/Pr1-4.4/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+int led = 14; 
+uint32_t *gpio_out = (uint32_t *)GPIO_OUT_REG;
+
+void setup() {                
+   pinMode(led, OUTPUT);   
+}
+
+void loop() {
+   *gpio_out |= (1 << led);
+   *gpio_out ^= (1 << led);
+}
